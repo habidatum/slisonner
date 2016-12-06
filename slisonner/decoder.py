c57c56510d9e2c0b0bf985932c4b2a88c4db564f
Nikita Pestrov <ipestriov@gmail.com>
2016-12-06 14:25:52 +0300

    Decoder to read from bytes

--- slisonner/decoder.py
+++ slisonner/decoder.py
@@ -1,34 +1,40 @@
-from os import path
 from zipfile import ZipFile
 import lz4f
 import numpy as np
 import json
+from io import BytesIO
 
 
-def decode_slison(filepath):
-    slison_id = parse_path(filepath)
+def decode_slison_bytes(slison_bytes):
+    decode_slison(BytesIO(slison_bytes))
 
-    with ZipFile(filepath) as slison_zip:
-        slice_meta = extract_slice_meta(slison_zip, slison_id)
+
+def decode_slison(file):
+    with ZipFile(file) as slison_zip:
+        meta_filename, data_filename = parse_slison_content(slison_zip)
+        slice_meta = extract_slice_meta(slison_zip, meta_filename)
         value_type = slice_meta['valueType']
-        slice_data = extract_slice_data(slison_zip, slison_id, value_type)
+        slice_data = extract_slice_data(slison_zip, data_filename,
+                                        value_type)
     return slice_data, slice_meta
 
 
-def parse_path(full_path):
-    slison_name = path.basename(full_path)
-    slison_id = slison_name.split('.')[0]
-    return slison_id
+def parse_slison_content(slison_zip):
+    slison_meta_file = [filename for filename in slison_zip.namelist()
+                        if '.json' in filename][0]
+    slison_data_file = [filename for filename in slison_zip.namelist()
+                        if '.slice' in filename][0]
+    return slison_meta_file, slison_data_file
 
 
-def extract_slice_meta(slison_zip, slison_id):
-    with slison_zip.open('{}.json'.format(slison_id)) as slice_meta_file:
+def extract_slice_meta(slison_zip, meta_filename):
+    with slison_zip.open(meta_filename) as slice_meta_file:
         meta = json.loads(slice_meta_file.read().decode())
     return meta
 
 
-def extract_slice_data(slison_zip, slison_id, value_type):
-    with slison_zip.open('{}.slice'.format(slison_id)) as slice_file:
+def extract_slice_data(slison_zip, data_filename, value_type):
+    with slison_zip.open(data_filename) as slice_file:
         data = lz4f.decompressFrame(slice_file.read(),
                                     dCtx=lz4f.createDecompContext())
         slice_data = np.frombuffer(data['decomp'], dtype=value_type)
