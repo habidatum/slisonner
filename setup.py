e8dd7f204debb115baadc8b8fdeb5f66604918e6
Nikita Pestrov <ipestriov@gmail.com>
2017-03-20 14:02:36 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(name='slisonner',
-      version='0.7.5 ',
+      version='0.7.6',
       description='Habidatum Platform Slison encode/decode utility',
       long_description='',
       author='Nikita Pestrov',
